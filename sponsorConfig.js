f9e4338a75931f1ae3a560bc66f7e76bdd81af1f
mauricedeschamps <mementomorikatsu@gmail.com>
2026-05-29 18:45:23 +0900

    sponsorConfig.js の更新

--- sponsorConfig.js
+++ sponsorConfig.js
@@ -3,8 +3,8 @@
 
 const sponsorConfig = {
   top: {
-    imageUrl: "",      // 例: "images/sponsor-top.png" または空文字の場合はテキスト表示
-    linkUrl: "",       // 例: "https://example.com"
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi5HADGJF4SNBdOn9zfuaD8quR38eYEzd5das2GEU6Yg&s=10",      // 例: "images/sponsor-top.png" または空文字の場合はテキスト表示
+    linkUrl: "https://www.shibuya-scramble-square.com/sky/",       // 例: "https://example.com"
     altText: "Top Sponsor",
     defaultText: "Advertisement Space (click to set)"  // 画像未設定時のテキスト
   },
